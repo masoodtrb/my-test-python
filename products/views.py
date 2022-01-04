e0c55efeda4e82ca446f3a6fd8f068c3b278717d
Masood Torabi <masood.trb@gmail.com>
2022-01-04 15:39:35 +0330

    init

---
+++ products/views.py
@@ -0,0 +1,20 @@
+from rest_framework import viewsets
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        pass
+
+    def load(self, request, pk=None):
+        pass
+    
+
+    def update(self, request, pk=None):
+        pass
+    
+
+    def destroy(self, request, pk=None):
+        pass
+    
